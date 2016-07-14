23e026ed1b54c52aeeb58b53854d4e05d9591e23
Alfredo Miranda <alfredo@miranda.io>
2016-07-14 17:15:50 -0500

    Query for flight info

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,27 @@
-console.log('hey there');
+window.onload = function() {
+  var searchBtn = document.getElementById('search');
+  var form = document.querySelector('form');
+
+  searchBtn.addEventListener('click', function(event) {
+    event.preventDefault();
+    var from = form.elements.from.value;
+    var to = form.elements.to.value;
+    var date = form.elements.date.value;
+    getFlightInfo(from, to, date).then(function(data) {
+      console.log(data);
+    });
+  });
+};
+
+
+function getFlightInfo(from, to, date) {
+  return new Promise(function(resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', '/search?from=' + from + '&to=' + to + '&date=' + date);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      resolve(xhr.response);
+    };
+    xhr.send();
+  });
+}
