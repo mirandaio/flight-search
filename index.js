fed8f7134ef30770c205facc8167746ee4ff3aa6
Alfredo Miranda <alfredo@miranda.io>
2016-07-14 00:18:59 -0500

    Add server

---
+++ index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+
+var app = express();
+
+app.use(express.static('public'));
+app.use(express.static('bower_components'));
+
+app.listen(process.env.PORT || 8764);
